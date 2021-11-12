15b2dd8138a27efc0dfae6db3f736b6c3f76ac59
Stiiipan <dumn2011@gmail.com>
2021-11-12 23:15:53 +0900

    Почти

--- z3/main.py
+++ z3/main.py
@@ -1,36 +1,68 @@
 import os
 import zipfile
+import gzip
 import sys
 import re
 from os import path, listdir
-
 import shutil
+import hashlib
+
+
+def id(path):
+    return hashlib.md5(path.encode()).hexdigest()[:6]
+
+
+def unZip(path, file):
+    with zipfile.ZipFile(path.replace("\\", "/") + "/" + file, 'r') as curZip:
+        curZip.extractall(path.replace("\\", "/") + "/" + id(path.replace("\\", "/") + "/" + file))
+
+
+def unZipAll(path):
+    for root, dirs, files in os.walk(path, topdown=False):
+        zips = [file for file in files if ".zip" in file]
+        for zip in zips:
+            # with zipfile.ZipFile(root.replace("\\", "/") + "/" + zip, 'r') as curZip:
+            #     curZip.extractall(root.replace("\\", "/") + "/" + id_generator())
+            unZip(root, zip)
+            os.remove(root.replace("\\", "/") + "/" + zip)
+
+    for root, dirs, files in os.walk(path, topdown=False):
+        gzs = [file for file in files if '.gz' in file]
+        for gz in gzs:
+            with gzip.open(root.replace("\\", "/") + "/" + gz, 'rb') as f:
+                file_content = f.read()
+                with open(root.replace("\\", "/") + "/" + gz + ".txt", 'w', encoding='utf-8') as newTxt:
+                    newTxt.write(file_content.decode('utf-8'))
+                print("---------------------")
+            os.remove(root.replace("\\", "/") + "/" + gz)
+
 
 if __name__ == '__main__':
     params = sys.argv
     filePath = params[1]
     savePath = "./tmp/"
     if len(params) > 2:
-        print("path")
         with zipfile.ZipFile(filePath, 'r') as myzip:
             savePath = params[2]
             myzip.extractall(savePath)
     else:
-        print("./")
         with zipfile.ZipFile(filePath, 'r') as myzip:
-            print(myzip)
             myzip.extractall(savePath)
 
-    print(savePath)
+    # print(savePath)
+    #
+    # print("start")
 
-    print("start")
+    unZipAll(savePath)
+    print("UNZIPPED_--------------------------------")
+    zipA = zipfile.ZipFile("./all.zip", "w")
+
+    uniqNumbers = set()
+    uniqEmails = set()
+
     for root, dirs, files in os.walk(savePath, topdown=False):
 
-        uniqNumbers = set()
-        uniqEmails = set()
-
         print("root", root)
-        print("dirs", dirs)
         print('files', files)
 
         txts = [file for file in files if ".txt" in file]
@@ -44,11 +76,12 @@ if __name__ == '__main__':
                 print(uniq)
                 for e in uniq:
                     numberString = str()
-                    for s in re.split(r'[ ,:;\t]', e):  # e.split(' '):  # #e.split(' '):
+                    for s in re.split(r'[ ,:;\t]+', e):  # e.split(' '):  # #e.split(' '):
                         if "@" in s:
                             if '.org' in s:
                                 uniqEmails.add(s)
                         elif len(s.strip()) > 0:
+
                             if s[0] == "(":
                                 ss = [x for x in s]
                                 if ss[1] == '1':
@@ -70,13 +103,11 @@ if __name__ == '__main__':
                 f.close()
                 pass
 
+        for filename in files:
+            filePath = os.path.join(root, filename)
+            zipA.write(filePath)
 
-
-
-
-
-
-
-
+    zipA.write("phones.txt")
+    zipA.write("emails.txt")
 
     # shutil.rmtree(savePath)
